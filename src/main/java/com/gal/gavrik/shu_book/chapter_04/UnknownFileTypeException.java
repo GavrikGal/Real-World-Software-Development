5455121f80d6ac99ecda389decb8c33ee6717b2c
GavrikGal <gavrik.gal@gmail.com>
2024-03-14 22:18:24 +0300

    Add chapter 04 example

---
+++ src/main/java/com/gal/gavrik/shu_book/chapter_04/UnknownFileTypeException.java
@@ -0,0 +1,8 @@
+package com.gal.gavrik.shu_book.chapter_04;
+
+
+public class UnknownFileTypeException extends RuntimeException {
+    public UnknownFileTypeException(final String message) {
+        super(message);
+    }
+}
