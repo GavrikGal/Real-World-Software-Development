d4c1437a823d9c8cd1575d993b91da2da276c304
GavrikGal <gavrik.gal@gmail.com>
2024-03-17 22:52:47 +0300

    Start chapter 05 example

---
+++ src/main/java/com/gal/gavrik/shu_book/chapter_05/Action.java
@@ -0,0 +1,6 @@
+package com.gal.gavrik.shu_book.chapter_05;
+
+@FunctionalInterface
+public interface Action {
+    void execute();
+}
---
+++ src/test/java/com/gal/gavrik/shu_book/chapter_05/BusinessRuleEngineTest.java
@@ -0,0 +1,26 @@
+package com.gal.gavrik.shu_book.chapter_05;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BusinessRuleEngineTest {
+
+    @Test
+    public void shouldHaveNoRulesInitially() {
+        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine();
+
+        assertEquals(0, businessRuleEngine.count());
+    }
+
+    @Test
+    public void shouldAddTwoActions() {
+        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine();
+
+        businessRuleEngine.addAction(() -> {});
+        businessRuleEngine.addAction(() -> {});
+
+        assertEquals(2, businessRuleEngine.count());
+
+    }
+}
